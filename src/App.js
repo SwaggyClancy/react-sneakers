04ff81776919a71172525bbe7745628aaeb32355
SwaggyClancy <swaggy_clancy@mail.ru>
2025-11-06 22:34:31 +0300

    SCSS added, some logos added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.svg" alt="Логотип" />
+          <div className="HeaderInfo">
+            <h3>React Sneakers</h3>
+            <p>Лучший магазин кроссовок</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <img width={18} height={18} src="/img/cart.svg" alt="Корзина" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img
+              width={18}
+              height={18}
+              src="/img/user.svg"
+              alt="Пользователь"
+            />
+          </li>
+        </ul>
       </header>
+      <main>
+        <h1>Все кроссовки</h1>
+      </main>
     </div>
   );
 }
