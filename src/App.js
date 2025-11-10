e272f685766e97efbb8c50416c688bd3a0cbe4ee
SwaggyClancy <swaggy_clancy@mail.ru>
2025-11-10 04:48:38 +0300

    Header made, item card ready

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 function App() {
   return (
     <div className="wrapper">
-      <header>
-        <div className="headerLeft">
-          <img width={40} height={40} src="/img/logo.svg" alt="Логотип" />
-          <div className="HeaderInfo">
-            <h3>React Sneakers</h3>
-            <p>Лучший магазин кроссовок</p>
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          {/* Header left part */}
+          <img
+            className="mr-20"
+            width={60}
+            height={60}
+            src="/img/logo.svg"
+            alt="Logo"
+          />
+          <div>
+            <h3 className="mb-0 text-uppercase">React Sneakers</h3>
+            <p className="mb-0 opacity-5">Лучший магазин кроссовок</p>
           </div>
         </div>
-        <ul className="headerRight">
-          <li>
-            <img width={18} height={18} src="/img/cart.svg" alt="Корзина" />
-            <span>1205 руб.</span>
-          </li>
-          <li>
+
+        <div className="d-flex align-center">
+          {/* Header right part */}
+          <span className="mr-30 d-flex">
             <img
-              width={18}
-              height={18}
-              src="/img/user.svg"
-              alt="Пользователь"
+              className="mr-10"
+              width={25}
+              height={25}
+              src="/img/cart.svg"
+              alt="Cart"
             />
-          </li>
-        </ul>
+            <span>1205 руб.</span>
+          </span>
+          <img width={25} height={25} src="/img/user.svg" alt="User" />
+        </div>
       </header>
-      <main>
-        <h1>Все кроссовки</h1>
+
+      <main className="content p-40">
+        <h1 className="mb-40 ">Все кроссовки</h1>
+        <div className="card">
+          <img
+            height={220}
+            width={210}
+            src="/sneakers/Nike_Air_Force_1.png"
+            alt="Adidas Forum 2000"
+          />
+          <h5>Adidas Forum 2000</h5>
+          <div className="d-flex justify-between align-center">
+            <div className="d-flex flex-column">
+              <span>Цена: </span>
+              <p>12 999 руб.</p>
+            </div>
+            <button className="button">
+              <img width={20} height={20} src="/img/plus.svg" alt="Plus" />
+            </button>
+          </div>
+        </div>
       </main>
     </div>
   );
